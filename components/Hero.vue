93149cc425a1b2adda8ca56de621e67d83b384ab
saidnahum <70162272+saidnahum@users.noreply.github.com>
2021-09-29 02:05:35 -0500

    Update Hero.vue

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,6 +1,6 @@
 <template>
    <div class="hero flex justify-left items-center bg-cover bg-center sm:bg-left pl-6" style="">
-      <div>
+      <div class="container mx-auto space-y-5">
          <h3 class="text-red-600 text-2xl sm:text-4xl uppercase font-bold">Nuevas Películas</h3>
          <h1 class="text-white my-2 sm:my-0 text-5xl sm:text-7xl mb-5"><span class="font-bold">Ahora</span> en Streaming</h1>
          <a href="#movie-grid" class="bg-red-600 text-white py-2 px-3 inline-block rounded mt-5 font-bold text-md">Ver Películas</a>
